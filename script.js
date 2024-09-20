1bcf68605d9670a6a3215c92977138353b60ba8b
AlbinoKaushik <albinokaushik6618@gmail.com>
2024-09-20 09:57:06 +0530

    improved

--- script.js
+++ script.js
@@ -1,30 +1,47 @@
 let container = document.querySelector("#container");
 let boxes = document.querySelectorAll(".box")
 
+const img =()=>{
+  let options = ["Apple","Banana","Cherry","Coconut","Mango","Orange","Peach","Watermelon","Apple","Banana","Cherry","Coconut","Mango","Orange","Peach","Watermelon"]
+  let randIndex = Math.floor(Math.random()*16)
+  // console.log(options[randIndex]); 
+  let a = options[randIndex];
+  // delete options[randIndex];
+  options.splice(randIndex,1)
+  let start =[""];
+  start += a
+  console.log(a);
+  console.log(start);
+  console.log(options);
+  return a,randIndex
+};
+
+
 const reset = ()=>{ boxes.forEach((box)=>{
-box.innerHTML = `<img src="Basket.jpg" alt="" width="80%" height="80%">`
+box.innerHTML = `<img src="Basket.jpg" alt="">`
 });
+img()
 };
 reset();
-const img =()=>{
-let options = ["Apple","Banana","Cherry","Coconut","Mango","Orange","Peach","Watermelon","Apple","Banana","Cherry","Coconut","Mango","Orange","Peach","Watermelon"]
-let randIndex = Math.floor(Math.random()*16)
-// console.log(options[randIndex]);
- const add = options[randIndex];
-// delete options[randIndex];
-options.splice(randIndex,1)
-  // console.log(add);
-//  console.log(options);
- return add,randIndex
-};
-boxes.forEach((box)=>{
-  img()
-  box.addEventListener("click",(add,randIndex)=>{
-    document.getElementById("box1")
-    box.innerHTML = `<img src="${randIndex}.jpg" alt="" width="80%" height="80%">`
-    if(){
-      
-    }
-        })
-    });
 
+
+boxes.forEach((box)=>{
+  box.addEventListener("click",(a,randIndex)=>{
+    document.getElementById("box1")
+    box.innerHTML = `<img src="${randIndex}.jpg" alt="">`
+  })
+});
+
+
+
+const game = ()=>{
+  if(box.innerHTML ===`<img src="Basket.jpg" alt=""></img>`){
+        
+  }
+  // else if(){
+    
+  // }
+  else{
+    reset();
+  }
+}
